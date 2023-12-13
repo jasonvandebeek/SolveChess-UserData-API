38e4aa4349cb445c912b85b2412fcbf60ba172f3
jasonvandebeek <jasonvandebeek@gmail.com>
2023-12-13 16:05:50 +0100

    Project Setup

--- API/Program.cs
+++ API/Program.cs
@@ -27,16 +27,13 @@ builder.Services.AddScoped<IUserService, UserService>(provider =>
     return new UserService(new UserDataDAL(dbContextOptions));
 });
 
-if (builder.Environment.IsDevelopment())
+builder.Services.AddCors(options =>
 {
-    builder.Services.AddCors(options =>
-    {
-        options.AddPolicy("AllowOrigin", builder => builder.WithOrigins("https://localhost:3000")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials());
-    });
-}
+    options.AddPolicy("AllowOrigin", builder => builder.WithOrigins("https://localhost:3000", "https://solvechess.xyz")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials());
+});
 
 builder.Services.AddAuthentication(options =>
 {
@@ -80,6 +77,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseCors("AllowOrigin");
